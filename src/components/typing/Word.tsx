bfb55ffe5fdca72fc871210eef5d559a01cf6e6b
stanl3yl33 <stanley32111@gmail.com>
2026-06-10 23:14:11 -0400

    feat: implemented Word component with per-character rendering

--- src/components/typing/Word.tsx
+++ src/components/typing/Word.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import { Character } from "./Character";
 
 type WordProps = {
   word: string;
-  inputIndex: number;
-  // word states include:
-  // * inprogress (empty or some words correct)
-  // * incorrect (invalid word)
-  // * finished (complete correct)
-  wordState: string;
+  typedWord: string; // user's input for the current word
+  isActive: boolean; // is the word being typed currently
 };
 
 /**
- * component that renders single word and style it based on correct / incorrect / in-progress
- * @param props
- * @returns
+ * component that renders single word and style it based on correctness
+ * It handles correct, incorrect, untyped, and extra characters typed
  */
-export function ComponentName(props: ComponentNameProps) {
-  return <div></div>;
+export function Word({ word, typedWord, isActive }: WordProps) {
+  // Handles case if user types more than the actual word count
+  const maxLen = Math.max(word.length, typedWord.length);
+
+  return (
+    <span className="inline-flex">
+      {Array.from({ length: maxLen }, (_, i) => {
+        const expectedChar = word[i];
+        const typedChar = typedWord[i];
+
+        return (
+          <Character
+            key={`${i}-${expectedChar ?? typedChar}`}
+            character={expectedChar} // typed pass expected char -> show input char
+            characterInput={typedChar}
+            isCursor={isActive && i === typedWord.length} // currently active word and up to last type character
+          />
+        );
+      })}
+    </span>
+  );
 }
